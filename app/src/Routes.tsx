8c07f057782b2d20f84742274982b49cb3bc9041
Tomy <132746200+popomaro516@users.noreply.github.com>
2024-09-19 01:22:45 +0000

    テンプレート作成

---
+++ app/src/Routes.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/home';
+const Routings: React.FC = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default Routings;
---
+++ app/src/pages/home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+    return (
+        <div>
+            <h1>ホーム画面</h1>
+            <p>ようこそ、ホーム画面へ！</p>
+        </div>
+    );
+};
+
+export default Home;
