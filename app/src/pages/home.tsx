4c3081192f8abf1215c805c2e79c9f80e336eafa
Tomy <132746200+popomaro516@users.noreply.github.com>
2024-09-21 13:10:35 +0000

    env:shadcn/uiの環境を構築した

--- app/src/pages/home.tsx
+++ app/src/pages/home.tsx
@@ -5,6 +5,9 @@ const Home: React.FC = () => {
         <div>
             <h1>ホーム画面</h1>
             <p>ようこそ、ホーム画面へ！</p>
+            <div className="bg-blue-500 text-white p-4">
+  This should be a blue box with white text and padding.
+</div>
         </div>
     );
 };
